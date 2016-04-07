7ba7bec8bfaa9cd0a745b1a25f32de829b09e182
Rachelle Faroul <rachelle.faroul@gmail.com>
2016-04-07 11:15:38 -0400

    get and log the tweets in console

--- index.js
+++ index.js
@@ -8,12 +8,26 @@ var client = new Twitter(creds); //creating a client object
 // 1. Get the user's followers with "followers/ids" endpoint
 //go to https://apps.twitter.com/ to get the info you need
 //REST API
-var getFollowers = function(name){
+var getFollowers = function(name, callback){
 	client.get("followers/ids", {screen_name: "rachellefaroul"}, function(err, response){
 		if (err) return console.log("Error!" + err);
 		console.log(response); //console log the response or do whatever it is you're doing with the response
 			//should see the ids for all of your followers
-		//callback(null, response.ids);
+		callback(null, response.ids); //the callback is the Stream API function below
 	});
 };
-getFollowers();
+
+//STREAM API
+//now that you've gotten a list of followers, watch them. 
+getFollowers("rachellefaroul", function(error, ids){
+	//for kanye bot would change follow to track: 'keyword' and statuses to "users/search" to look for kanye activity
+	//2. connect to stream for followers using "statuses/filter"
+	client.stream("statuses/filter", { follow: ids.join() }, function(stream){
+		stream.on("error", function(error) { 
+			console.log(error) 
+		});
+		stream.on("data", function(tweet){
+			console.log(tweet.text);
+		});
+	})
+});
